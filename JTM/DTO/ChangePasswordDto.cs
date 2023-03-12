f4364114185f6f69ff7dbb3ddcf07e1ff8ce0592
Jakub <42150609+JacquesM27@users.noreply.github.com>
2023-03-12 20:47:30 +0100

    add dto and model

---
+++ JTM/DTO/ChangePasswordDto.cs
@@ -0,0 +1,9 @@
+﻿namespace JTM.DTO
+{
+    public class ChangePasswordDto
+    {
+        public int UserId { get; set; }
+        public string Password { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+    }
+}
