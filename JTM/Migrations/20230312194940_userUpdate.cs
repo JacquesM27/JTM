07989a84a00132dc6f872f8f559769e2aea2db45
Jakub <42150609+JacquesM27@users.noreply.github.com>
2023-03-12 20:50:05 +0100

    update db

---
+++ JTM/Migrations/20230312194940_userUpdate.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JTM.Migrations
+{
+    public partial class userUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordResetToken",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordResetToken",
+                table: "Users");
+        }
+    }
+}
