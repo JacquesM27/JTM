53a0aec57a8f6dd0a300e4cc36162aacc96f965b
Jakub <42150609+JacquesM27@users.noreply.github.com>
2023-03-19 18:38:45 +0100

    prepare projekt to integration tests

---
+++ JTM.IntegrationTests/Helpers/CleanTestDatabaseToTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace JTM.IntegrationTests.Helpers
+{
+    public class CleanTestDatabaseToTest
+    {
+        private static async Task CleanDb(string connectionString)
+        {
+            string query = @"ALTER TABLE [dbo].[WorkingTimes] DROP CONSTRAINT [FK_WorkingTimes_Users_EmployeeId]
+
+                            truncate table dbo.users
+
+                            ALTER TABLE [dbo].[WorkingTimes]  WITH CHECK ADD  CONSTRAINT [FK_WorkingTimes_Users_EmployeeId] FOREIGN KEY([EmployeeId])
+                            REFERENCES [dbo].[Users] ([Id])
+                            ON DELETE CASCADE
+                            ALTER TABLE [dbo].[WorkingTimes] CHECK CONSTRAINT [FK_WorkingTimes_Users_EmployeeId]";
+            using SqlConnection connection = new(connectionString);
+            SqlCommand command = new(query, connection);
+            await command.Connection.OpenAsync();
+            await command.ExecuteNonQueryAsync();
+        }
+    }
+}
---
+++ JTM.IntegrationTests/Helpers/TestDapperConnectionFactory.cs
@@ -0,0 +1,18 @@
+﻿using JTM.Data.DapperConnection;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace JTM.IntegrationTests.Helpers
+{
+    public class TestDapperConnectionFactory : IDapperConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public TestDapperConnectionFactory(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("TestConnectionString");
+        }
+
+        public IDbConnection DbConnection => new SqlConnection(_connectionString);
+    }
+}
