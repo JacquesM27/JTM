a8e59339270ee8c0274ab92d9db9f9c208e92e17
Jakub <42150609+JacquesM27@users.noreply.github.com>
2023-03-23 21:31:24 +0100

    Create HttpCookieHelper.cs

---
+++ JTM.IntegrationTests/Helpers/HttpCookieHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Net.Http.Headers;
+
+namespace JTM.IntegrationTests.Helpers
+{
+    public static class HttpCookieHelper
+    {
+        public static IRequestCookieCollection MockRequestCookieCollection(string key, string value)
+        {
+            var requestFeature = new HttpRequestFeature();
+            var featureCollection = new FeatureCollection();
+
+            requestFeature.Headers = new HeaderDictionary
+            {
+                { HeaderNames.Cookie, new StringValues(key + "=" + value) }
+            };
+
+            featureCollection.Set<IHttpRequestFeature>(requestFeature);
+
+            var cookiesFeature = new RequestCookiesFeature(featureCollection);
+
+            return cookiesFeature.Cookies;
+        }
+    }
+}
