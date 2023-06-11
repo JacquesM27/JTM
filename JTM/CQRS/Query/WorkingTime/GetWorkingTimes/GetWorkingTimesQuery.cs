1819dede18aa87017292f83004df993f6afd6dbc
Jakub <42150609+JacquesM27@users.noreply.github.com>
2023-06-11 21:52:08 +0200

    added query for working times

---
+++ JTM/CQRS/Query/WorkingTime/GetWorkingTimes/GetWorkingTimesQuery.cs
@@ -0,0 +1,27 @@
+﻿using JTM.DTO.WorkingTime;
+using MediatR;
+
+namespace JTM.CQRS.Query.WorkingTime.GetWorkingTimes
+{
+    public class GetWorkingTimesQuery : IRequest<IEnumerable<BasicWorkingTimeDto>>
+    {
+        public IEnumerable<int>? EmployeesId { get; init; }
+        public DateTime? StartDate { get; init; }
+        public DateTime? EndDate { get; init; }
+        public IEnumerable<int>? CompaniesId { get; init; }
+        public string? NoteSearchPhrase { get; init; }
+        public int PageNumber { get; init; }
+        public int PageSize { get; init; }
+
+        public GetWorkingTimesQuery(IEnumerable<int>? employeesId, DateTime? startDate, DateTime? endDate, IEnumerable<int>? companiesId, string? noteSearchPhrase, int pageNumber, int pageSize)
+        {
+            EmployeesId = employeesId;
+            StartDate = startDate;
+            EndDate = endDate;
+            CompaniesId = companiesId;
+            NoteSearchPhrase = noteSearchPhrase;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+        }
+    }
+}
---
+++ JTM/CQRS/Query/WorkingTime/GetWorkingTimes/GetWorkingTimesQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using JTM.DTO.WorkingTime;
+using MediatR;
+
+namespace JTM.CQRS.Query.WorkingTime.GetWorkingTimes
+{
+    public class GetWorkingTimesQueryHandler : IRequestHandler<GetWorkingTimesQuery, IEnumerable<BasicWorkingTimeDto>>
+    {
+        public Task<IEnumerable<BasicWorkingTimeDto>> Handle(GetWorkingTimesQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
