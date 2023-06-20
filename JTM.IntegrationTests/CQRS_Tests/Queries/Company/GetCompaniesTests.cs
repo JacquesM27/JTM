47f175e6f8e0dfb13cf2d03711c29a3594b583b9
Jakub <42150609+JacquesM27@users.noreply.github.com>
2023-06-20 20:59:09 +0200

    created get companies tests

---
+++ JTM.IntegrationTests/CQRS_Tests/Queries/Company/GetCompaniesTests.cs
@@ -0,0 +1,53 @@
+﻿using JTM.CQRS.Query.Company;
+using JTM.DTO.Company;
+using Moq;
+using Model = JTM.Data.Model;
+
+namespace JTM.IntegrationTests.CQRS_Tests.Queries.Company
+{
+    public class GetCompaniesTests : CompanyTestBase
+    {
+        [Fact]
+        public async Task GetCompanies_ForNoneCompany_ShouldReturnEmptyCollection()
+        {
+            // Arrange
+            MockUnitOfWork
+                .Setup(c => c.CompanyRepository.QueryAsync(null))
+                .ReturnsAsync(new List<Model.Company>());
+            var command = new GetCompaniesQuery();
+            var commandHandler = new GetCompaniesQueryHandler(MockUnitOfWork.Object);
+
+            // Act
+            var result = await commandHandler.Handle(command, default);
+
+            // Assert
+            Assert.Empty(result);
+            Assert.NotNull(result);
+            Assert.IsAssignableFrom<IEnumerable<CompanyDto>>(result);
+        }
+
+        [Fact]
+        public async Task GetCompanies_ForTwoCompaniesWithDetails_ShouldReturnIEnumerableWithTwoElements()
+        {
+            // Arrange
+            var companies = new List<Model.Company>
+            {
+                new Model.Company { Id = 1, Name = "Test" },
+                new Model.Company { Id = 2, Name = "Test2" }
+            };
+            MockUnitOfWork
+                .Setup(c => c.CompanyRepository.QueryAsync(null))
+                .ReturnsAsync(companies);
+            var command = new GetCompaniesQuery();
+            var commandHandler = new GetCompaniesQueryHandler(MockUnitOfWork.Object);
+
+            // Act
+            var result = await commandHandler.Handle(command, default);
+
+            // Assert
+            Assert.NotEmpty(result);
+            Assert.NotNull(result);
+            Assert.IsAssignableFrom<IEnumerable<CompanyDto>>(result);
+        }
+    }
+}
