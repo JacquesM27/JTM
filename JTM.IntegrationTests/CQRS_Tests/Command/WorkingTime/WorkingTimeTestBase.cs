0e4c467b6cc3dc7c31170a0d47d747f73feb546f
Jakub <42150609+JacquesM27@users.noreply.github.com>
2023-06-20 20:29:46 +0200

    Create WorkingTimeTestBase.cs

---
+++ JTM.IntegrationTests/CQRS_Tests/Command/WorkingTime/WorkingTimeTestBase.cs
@@ -0,0 +1,15 @@
+﻿using JTM.Data.UnitOfWork;
+using Moq;
+
+namespace JTM.IntegrationTests.CQRS_Tests.Command.WorkingTime
+{
+    public class WorkingTimeTestBase
+    {
+        protected readonly Mock<IUnitOfWork> MockUnitOfWork;
+
+        public WorkingTimeTestBase()
+        {
+            MockUnitOfWork = new();
+        }
+    }
+}
