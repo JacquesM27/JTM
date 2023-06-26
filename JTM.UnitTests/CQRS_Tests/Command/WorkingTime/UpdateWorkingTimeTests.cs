56793096fa616118020082673c258ef3cdfd21bc
Jakub <42150609+JacquesM27@users.noreply.github.com>
2023-06-26 21:42:02 +0200

    finished unit tests for update working time

--- JTM.UnitTests/CQRS_Tests/Command/WorkingTime/UpdateWorkingTimeTests.cs
+++ JTM.UnitTests/CQRS_Tests/Command/WorkingTime/UpdateWorkingTimeTests.cs
@@ -1,18 +1,14 @@
 ﻿using JTM.CQRS.Command.WorkingTime;
 using JTM.Exceptions;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Model = JTM.Data.Model;
 
 namespace JTM.UnitTests.CQRS_Tests.Command.WorkingTime
 {
     public class UpdateWorkingTimeTests : UnitTestBase
     {
         [Fact]
-        public async Task UpdateWorkingTime_ForDiffrentIds_ShouldThrowWorkingTImeException()
+        public async Task UpdateWorkingTime_ForDiffrentIds_ShouldThrowWorkingTimeException()
         {
             // Arrange
             int tmpHeaderId = 1;
@@ -35,6 +31,199 @@ namespace JTM.UnitTests.CQRS_Tests.Command.WorkingTime
             // Assert
             var exception = await Assert.ThrowsAsync<WorkingTimeException>(HandleCommand);
             Assert.Equal($"Id from header({tmpHeaderId}) does not equal id from route({tmpRouteId}).", exception.Message);
+        }
+
+        [Fact]
+        public async Task UpdateWorkingTime_ForNotExistWorkingTime_ShouldThrowWorkingTimeException()
+        {
+            // Arrange
+            int tmpId = 1;
+            MockUnitOfWork
+                .Setup(x => x.WorkingTimeRepository.GetByIdAsync(tmpId))
+                .Returns(Task.FromResult<Model.WorkingTime?>(null));
+
+            var command = new UpdateWorkingTimeCommand(
+               headerId: tmpId,
+               routeId: tmpId,
+               workingDate: It.IsAny<DateTime>(),
+               secondsOfWork: It.IsAny<int>(),
+               note: It.IsAny<string>(),
+               companyId: It.IsAny<int>(),
+               employeeId: It.IsAny<int>(),
+               editorId: It.IsAny<int>()
+               );
+            var commandHandler = new UpdateWorkingTimeCommandHandler(MockUnitOfWork.Object);
+
+            // Act
+            async Task HandleCommand() => await commandHandler.Handle(command, default);
+
+            // Assert
+            var exception = await Assert.ThrowsAsync<WorkingTimeException>(HandleCommand);
+            Assert.Equal($"Working time with id: {tmpId} does not exist.", exception.Message);
+        }
+
+        [Fact]
+        public async Task UpdateWorkingTime_ForNotExistWorkingEmployee_ShouldThrowWorkingTimeException()
+        {
+            // Arrange
+            MockUnitOfWork
+                .Setup(x => x.WorkingTimeRepository.GetByIdAsync(It.IsAny<int>()))
+                .Returns(Task.FromResult<Model.WorkingTime?>(new Model.WorkingTime()));
+            int tmpEmployeeId = 1;
+            MockUnitOfWork
+                .Setup(x => x.UserRepository.AnyAsync(tmpEmployeeId))
+                .Returns(Task.FromResult(false));
+
+            var command = new UpdateWorkingTimeCommand(
+              headerId: It.IsAny<int>(),
+              routeId: It.IsAny<int>(),
+              workingDate: It.IsAny<DateTime>(),
+              secondsOfWork: It.IsAny<int>(),
+              note: It.IsAny<string>(),
+              companyId: It.IsAny<int>(),
+              employeeId: tmpEmployeeId,
+              editorId: It.IsAny<int>()
+              );
+            var commandHandler = new UpdateWorkingTimeCommandHandler(MockUnitOfWork.Object);
+
+            // Act
+            async Task HandleCommand() => await commandHandler.Handle(command, default);
+
+            // Assert
+            var exception = await Assert.ThrowsAsync<WorkingTimeException>(HandleCommand);
+            Assert.Equal($"User with id:{tmpEmployeeId} does not exist.", exception.Message);
+        }
+
+        [Fact]
+        public async Task UpdateWorkingTime_ForNotExistWorkingAuthor_ShouldThrowWorkingTimeException()
+        {
+            // Arrange
+            MockUnitOfWork
+                .Setup(x => x.WorkingTimeRepository.GetByIdAsync(It.IsAny<int>()))
+                .Returns(Task.FromResult<Model.WorkingTime?>(new Model.WorkingTime()));
+            int tmpEmployeeId = 2;
+            MockUnitOfWork
+               .Setup(x => x.UserRepository.AnyAsync(tmpEmployeeId))
+               .Returns(Task.FromResult(true));
+            int tmpEditorId = 1;
+            MockUnitOfWork
+                .Setup(x => x.UserRepository.AnyAsync(tmpEditorId))
+                .Returns(Task.FromResult(false));
+
+            var command = new UpdateWorkingTimeCommand(
+              headerId: It.IsAny<int>(),
+              routeId: It.IsAny<int>(),
+              workingDate: It.IsAny<DateTime>(),
+              secondsOfWork: It.IsAny<int>(),
+              note: It.IsAny<string>(),
+              companyId: It.IsAny<int>(),
+              employeeId: tmpEmployeeId,
+              editorId: tmpEditorId
+              );
+            var commandHandler = new UpdateWorkingTimeCommandHandler(MockUnitOfWork.Object);
+
+            // Act
+            async Task HandleCommand() => await commandHandler.Handle(command, default);
+
+            // Assert
+            var exception = await Assert.ThrowsAsync<WorkingTimeException>(HandleCommand);
+            Assert.Equal($"User with id:{tmpEditorId} does not exist.", exception.Message);
+        }
+
+        [Fact]
+        public async Task UpdateWorkingTime_ForNotExistCompany_ShouldThrowWorkingTimeException()
+        {
+            // Arrange
+            MockUnitOfWork
+                .Setup(x => x.WorkingTimeRepository.GetByIdAsync(It.IsAny<int>()))
+                .Returns(Task.FromResult<Model.WorkingTime?>(new Model.WorkingTime()));
+            MockUnitOfWork
+               .Setup(x => x.UserRepository.AnyAsync(It.IsAny<int>()))
+               .Returns(Task.FromResult(true));
+            int tmpCompanyId = 1;
+            MockUnitOfWork
+                .Setup(x => x.CompanyRepository.AnyAsync(tmpCompanyId))
+                .Returns(Task.FromResult(false));
+
+            var command = new UpdateWorkingTimeCommand(
+              headerId: It.IsAny<int>(),
+              routeId: It.IsAny<int>(),
+              workingDate: It.IsAny<DateTime>(),
+              secondsOfWork: It.IsAny<int>(),
+              note: It.IsAny<string>(),
+              companyId: tmpCompanyId,
+              employeeId: It.IsAny<int>(),
+              editorId: It.IsAny<int>()
+              );
+            var commandHandler = new UpdateWorkingTimeCommandHandler(MockUnitOfWork.Object);
+
+            // Act
+            async Task HandleCommand() => await commandHandler.Handle(command, default);
+
+            // Assert
+            var exception = await Assert.ThrowsAsync<WorkingTimeException>(HandleCommand);
+            Assert.Equal($"Company with id:{tmpCompanyId} does not exist.", exception.Message);
+        }
+
+        [Fact]
+        public async Task UpdateWorkingTime_ForValidModelWithCompany_ShouldPassWithoutException()
+        {
+            // Arrange
+            MockUnitOfWork
+                .Setup(x => x.WorkingTimeRepository.GetByIdAsync(It.IsAny<int>()))
+                .Returns(Task.FromResult<Model.WorkingTime?>(new Model.WorkingTime()));
+            MockUnitOfWork
+               .Setup(x => x.UserRepository.AnyAsync(It.IsAny<int>()))
+               .Returns(Task.FromResult(true));
+            MockUnitOfWork
+                .Setup(x => x.CompanyRepository.AnyAsync(It.IsAny<int>()))
+                .Returns(Task.FromResult(true));
+
+            var command = new UpdateWorkingTimeCommand(
+              headerId: It.IsAny<int>(),
+              routeId: It.IsAny<int>(),
+              workingDate: It.IsAny<DateTime>(),
+              secondsOfWork: It.IsAny<int>(),
+              note: It.IsAny<string>(),
+              companyId: It.IsAny<int>(),
+              employeeId: It.IsAny<int>(),
+              editorId: It.IsAny<int>()
+              );
+            var commandHandler = new UpdateWorkingTimeCommandHandler(MockUnitOfWork.Object);
+
+            // Act
+            await commandHandler.Handle(command, default);
+
+            // Assert
+        }
+
+        [Fact]
+        public async Task UpdateWorkingTime_ForValidModelWithoutCompany_ShouldPassWithoutException()
+        {
+            // Arrange
+            MockUnitOfWork
+                .Setup(x => x.WorkingTimeRepository.GetByIdAsync(It.IsAny<int>()))
+                .Returns(Task.FromResult<Model.WorkingTime?>(new Model.WorkingTime()));
+            MockUnitOfWork
+               .Setup(x => x.UserRepository.AnyAsync(It.IsAny<int>()))
+               .Returns(Task.FromResult(true));
+
+            var command = new UpdateWorkingTimeCommand(
+              headerId: It.IsAny<int>(),
+              routeId: It.IsAny<int>(),
+              workingDate: It.IsAny<DateTime>(),
+              secondsOfWork: It.IsAny<int>(),
+              note: It.IsAny<string>(),
+              companyId: null,
+              employeeId: It.IsAny<int>(),
+              editorId: It.IsAny<int>()
+              );
+            var commandHandler = new UpdateWorkingTimeCommandHandler(MockUnitOfWork.Object);
+
+            // Act
+            await commandHandler.Handle(command, default);
+
+            // Assert
         }
     }
 }
