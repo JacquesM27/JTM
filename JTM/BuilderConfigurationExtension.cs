54c0efb9dcce4eaeeb4e3c6e79005db105e82096
Jakub <42150609+JacquesM27@users.noreply.github.com>
2023-06-27 20:12:56 +0200

    moved registering to custom extension class

---
+++ JTM/BuilderConfigurationExtension.cs
@@ -0,0 +1,93 @@
+﻿using FluentValidation;
+using JTM.Data;
+using JTM.Data.UnitOfWork;
+using JTM.DTO.Account.RegisterUser;
+using JTM.DTO.Company.AddCompany;
+using JTM.DTO.Company.UpdateCompany;
+using JTM.DTO.WorkingTime.AddWorkingTime;
+using JTM.DTO.WorkingTime.UpdateWorkingTime;
+using JTM.Middleware;
+using JTM.Services.RabbitService;
+using JTM.Services.TokenService;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Reflection;
+
+namespace JTM
+{
+    public static class BuilderConfigurationExtension
+    {
+        public static void ConfigureSwagger(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Description = "Standard Auth header - use Bearer scheme: \"Bearer {token}\"",
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                options.OperationFilter<SecurityRequirementsOperationFilter>();
+            });
+        }
+
+        public static void ConfigureMiddlewareServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddTransient<ExceptionHandlingMessage>();
+        }
+
+        public static void ConfigureMediatr(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+        }
+
+        public static void ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSingleton<IRabbitService, RabbitService>();
+            builder.Services.AddScoped<ITokenService, TokenService>();
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+
+        public static void ConfigureDbContext(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+        }
+
+        public static void ConfigureAuthentication(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8
+                            .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+                        ValidateIssuer = true,
+                        ValidIssuer = "secret key",
+                        ValidateAudience = true,
+                        ValidAudience = "secret key2"
+                    };
+                });
+        }
+
+        public static void ConfigureValidators(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<IValidator<RegisterUserDto>, RegisterUserDtoValidator>();
+            builder.Services.AddScoped<IValidator<AddCompanyDto>, AddCompanyDtoValidator>();
+            builder.Services.AddScoped<IValidator<UpdateCompanyDto>, UpdateCompanyDtoValidator>();
+            builder.Services.AddScoped<IValidator<AddWorkingTimeDto>, AddWorkingTimeDtoValidator>();
+            builder.Services.AddScoped<IValidator<UpdateWorkingTimeDto>, UpdateWorkingTimeDtoValidator>();
+        }
+
+        public static void UseRegisteredMiddlewares(this WebApplication app)
+        {
+            app.UseMiddleware<ExceptionHandlingMessage>();
+        }
+    }
+}
--- JTM/Program.cs
+++ JTM/Program.cs
@@ -1,20 +1,4 @@
-using FluentValidation;
-using JTM.Data;
-using JTM.Data.UnitOfWork;
-using JTM.DTO.Account.RegisterUser;
-using JTM.DTO.Company.AddCompany;
-using JTM.DTO.Company.UpdateCompany;
-using JTM.DTO.WorkingTime.AddWorkingTime;
-using JTM.DTO.WorkingTime.UpdateWorkingTime;
-using JTM.Middleware;
-using JTM.Services.RabbitService;
-using JTM.Services.TokenService;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.Filters;
-using System.Reflection;
+using JTM;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,69 +7,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-    {
-        Description = "Standard Auth header - use Bearer scheme: \"Bearer {token}\"",
-        In = ParameterLocation.Header,
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey
-    });
-    options.OperationFilter<SecurityRequirementsOperationFilter>();
-});
 
-builder.Services.AddTransient<ExceptionHandlingMessage>();
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
-
-builder.Services.AddSingleton<IRabbitService, RabbitService>();
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
+builder.ConfigureSwagger();
+builder.ConfigureMiddlewareServices();
+builder.ConfigureMediatr();
+builder.ConfigureMediatr();
 builder.Services.AddHttpContextAccessor();
-
-builder.Services.AddDbContext<DataContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8
-                .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
-            ValidateIssuer = true,
-            ValidIssuer = "secret key",
-            ValidateAudience = true,
-            ValidAudience = "secret key2"
-        };
-    });
-
-builder.Services.AddScoped<IValidator<RegisterUserDto>, RegisterUserDtoValidator>();
-builder.Services.AddScoped<IValidator<AddCompanyDto>, AddCompanyDtoValidator>();
-builder.Services.AddScoped<IValidator<UpdateCompanyDto>, UpdateCompanyDtoValidator>();
-builder.Services.AddScoped<IValidator<AddWorkingTimeDto>, AddWorkingTimeDtoValidator>();
-builder.Services.AddScoped<IValidator<UpdateWorkingTimeDto>, UpdateWorkingTimeDtoValidator>();
+builder.ConfigureDbContext();
+builder.ConfigureAuthentication();
+builder.ConfigureValidators();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
 app.UseSwagger();
 app.UseSwaggerUI();
 //}
-
 app.UseHttpsRedirection();
-
 app.UseAuthentication();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
-app.UseMiddleware<ExceptionHandlingMessage>();
-
+app.UseRegisteredMiddlewares();
 app.Run();
 
 public partial class Program { }
