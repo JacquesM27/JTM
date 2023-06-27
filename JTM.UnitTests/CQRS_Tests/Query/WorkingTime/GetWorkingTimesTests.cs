c6e4cde98328debc763381d76156e41752bef7c0
Jakub <42150609+JacquesM27@users.noreply.github.com>
2023-06-27 19:42:39 +0200

    create test for get working times command handler

---
+++ JTM.UnitTests/CQRS_Tests/Query/WorkingTime/GetWorkingTimesTests.cs
@@ -0,0 +1,44 @@
+﻿using JTM.CQRS.Query.WorkingTime;
+using JTM.DTO.WorkingTime;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Model = JTM.Data.Model;
+
+namespace JTM.UnitTests.CQRS_Tests.Query.WorkingTime
+{
+    public class GetWorkingTimesTests : UnitTestBase
+    {
+        [Fact]
+        public async Task GetWorkingTimes_ForAnyData_ShouldReturnEmptyCollection()
+        {
+            // Arrange
+            MockUnitOfWork
+                .Setup(c => c.WorkingTimeRepository.QueryAsync(
+                        It.IsAny<Expression<Func<Model.WorkingTime, bool>>>(),
+                        It.IsAny<Expression<Func<Model.WorkingTime, object>>[]>()))
+                .Returns(Task.FromResult(Enumerable.Empty<Model.WorkingTime>()));
+
+            var command = new GetWorkingTimesQuery(
+                It.IsAny<IEnumerable<int>>(),
+                It.IsAny<DateTime>(),
+                It.IsAny<DateTime>(),
+                It.IsAny<IEnumerable<int>>(),
+                string.Empty,
+                It.IsAny<int>(),
+                It.IsAny<int>());
+            var commandHandler = new GetWorkingTimesQueryHandler(MockUnitOfWork.Object);
+
+            // Act
+            var result = await commandHandler.Handle(command, default);
+
+            // Assert
+            Assert.Empty(result);
+            Assert.IsAssignableFrom<IEnumerable<BasicWorkingTimeDto>>(result);
+        }
+    }
+}
