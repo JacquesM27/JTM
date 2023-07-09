32651855a091fb0ef2737a676e19656fd7e4dd45
Jakub <42150609+JacquesM27@users.noreply.github.com>
2023-07-09 17:18:44 +0200

    Update HttpContentHelper.cs

--- JTM.IntegrationTests/Helpers/HttpContentHelper.cs
+++ JTM.IntegrationTests/Helpers/HttpContentHelper.cs
@@ -11,5 +11,12 @@ namespace JTM.IntegrationTests.Helpers
             var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
             return httpContent;
         }
+
+        public static async Task<T?> FromJsonHttpResponseMessage<T>(this HttpResponseMessage httpResponseMessage)
+        {
+            string json = await httpResponseMessage.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<T>(json);
+            return result;
+        }
     }
 }
